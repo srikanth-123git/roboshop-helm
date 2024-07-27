8963772e218b0b237dbc187eff580d738fc02017
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-27 14:34:51 +0530

    Add helm charts install

---
+++ infra-helm-charts/install.sh
@@ -0,0 +1,6 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
+
+exit
+kubectl create ns argocd
+kubectl apply -f argo-dev.yaml -n argocd
