102a8db8cf22a83bb7b2a2902416bb529814a99e
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-27 20:37:42 +0530

    Add helm charts install

--- infra-helm-charts/install.sh
+++ infra-helm-charts/install.sh
@@ -1,3 +1,4 @@
+aws eks update-kubeconfig --name dev-eks
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 
