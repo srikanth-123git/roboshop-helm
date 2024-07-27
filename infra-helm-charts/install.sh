b9b72f1ca8d0fb0c260e24b286c16bf64f023421
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-27 21:07:35 +0530

    Add helm charts install

--- infra-helm-charts/install.sh
+++ infra-helm-charts/install.sh
@@ -2,6 +2,16 @@ aws eks update-kubeconfig --name dev-eks
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 
-exit
+LOAD_BALANCER=$(kubectl get svc ngx-ingres-ingress-nginx-controller | grep ngx-ingres-ingress-nginx-controller | awk '{print $4}')
+
+while true ; do
+  echo "Waiting for Load Balancer to come to Active"
+  nslookup $LOAD_BALANCER &>/dev/null
+  if [ $? -eq 0 ]; then break ; fi
+  sleep 5
+done
+
 kubectl create ns argocd
 kubectl apply -f argo-dev.yaml -n argocd
+
+
