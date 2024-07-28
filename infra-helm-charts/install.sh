552b5b7b9a9bb18ccd7ba7c304d98eba17567334
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-28 16:19:35 +0530

    External DNS setup

--- infra-helm-charts/install.sh
+++ infra-helm-charts/install.sh
@@ -11,8 +11,8 @@ while true ; do
   sleep 5
 done
 
-kubectl apply -f external-dns-dev.yaml
-sleep 15
+#kubectl apply -f external-dns-dev.yaml
+#sleep 15
 
 kubectl create ns argocd
 kubectl apply -f argo-dev.yaml -n argocd
