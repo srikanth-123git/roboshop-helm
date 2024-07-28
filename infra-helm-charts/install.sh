893279c116bb6831a169500eb0d74257245b6051
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-28 11:01:55 +0530

    External DNS setup

--- infra-helm-charts/install.sh
+++ infra-helm-charts/install.sh
@@ -2,20 +2,21 @@ aws eks update-kubeconfig --name dev-eks
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 
-LOAD_BALANCER=$(kubectl get svc ngx-ingres-ingress-nginx-controller | grep ngx-ingres-ingress-nginx-controller | awk '{print $4}')
-
-while true ; do
-  echo "Waiting for Load Balancer to come to Active"
-  nslookup $LOAD_BALANCER &>/dev/null
-  if [ $? -eq 0 ]; then break ; fi
-  sleep 5
-done
-
-kubectl apply -f external-dns-dev.yaml
-sleep 15
-
-kubectl create ns argocd
-kubectl apply -f argo-dev.yaml -n argocd
+exit
+#LOAD_BALANCER=$(kubectl get svc ngx-ingres-ingress-nginx-controller | grep ngx-ingres-ingress-nginx-controller | awk '{print $4}')
+#
+#while true ; do
+#  echo "Waiting for Load Balancer to come to Active"
+#  nslookup $LOAD_BALANCER &>/dev/null
+#  if [ $? -eq 0 ]; then break ; fi
+#  sleep 5
+#done
+#
+#kubectl apply -f external-dns-dev.yaml
+#sleep 15
+#
+#kubectl create ns argocd
+#kubectl apply -f argo-dev.yaml -n argocd
 
 
 
