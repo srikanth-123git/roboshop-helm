fe54b067e17ebb23c51244f38df0f2aa6675f264
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-28 18:12:17 +0530

    Helm chart for roboshop

---
+++ argo.sh
@@ -0,0 +1,5 @@
+argocd login $(kubectl get ingress -A | grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+
+for app in frontend ; do
+    argocd app create ${app}  --repo https://github.com/srikanth-123git/roboshop-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
+done
